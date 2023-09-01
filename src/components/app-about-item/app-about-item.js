a82fec0b1ea0ae60554dadbf665cab90a11f2db6
Dmitry Domnin <domnin.dm>
2023-09-01 19:27:00 +0300

    The block about application was rebuilt, styles were added

--- src/components/app-about-item/app-about-item.js
+++ src/components/app-about-item/app-about-item.js
@@ -1,31 +1,21 @@
 import './app-about-item.css';
-import {Component} from 'react';
-import {Col} from 'react-bootstrap';
 
-class AppAboutItem extends Component {
-    constructor (props) {
-        super(props);
-    }
-
-    render () {
-        let {pic, text} = this.props;
-
-        return (
-            <Col lg={6}
-                 className='item_wrapper'>
-                    <div className='about_wrapper'>
-                        <div className = 'about_img_container'>
-                            <img src={pic} width='50px' height='50px'/>
-                        </div>
-                        <div className='about_text_container'>
-                            {text}  
-                        </div>
-                    </div>
-            </Col>
-        )
+function AppAboutItem (props) {
+    
+    const {pic, text} = props;
         
-    };
-}
+    return (
+        <div className='item_wrapper'>
+            <div className='about_wrapper'>
+                <div className = 'about_img_container'>
+                    <img src={pic} width='50px' height='50px'/>
+                </div>
+                <div className='about_text_container'>
+                    {text}  
+                </div>
+            </div>
+        </div>
+    )  
+};
 
-// pt-4 pb-4
 export default AppAboutItem;
--- src/components/app-about/app-about.js
+++ src/components/app-about/app-about.js
@@ -4,19 +4,24 @@ import studyingPic from './study_icon_2.png';
 import filterPic from './filter_icon_5.png';
 import freePic from './free_icon.png';
 import categoryPic from './categories_icon.png';
-import {Component} from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
 
 function AppAbout () {
+    
     return ( 
-            <Container className='about_block'>
-                <Row>
-                    <AppAboutItem pic={studyingPic} text={'World of Words - это приложение для изучения иностранных слов. Добавляйте и удаляйте слова, а также перевод к ним, пополняя словарь.'}> </AppAboutItem>
-                    <AppAboutItem pic={filterPic} text={'Работать со словарем легко благодаря ряду фильтров - сортируйте слова по дате добавления и категории. '}> </AppAboutItem>
-                    <AppAboutItem pic={freePic} text={'Приложение абсолютно бесплатно - добавляйте любое количество слов и пользуйтесь словарем в любое время с разных устройств.'}> </AppAboutItem>
-                    <AppAboutItem pic={categoryPic} text={'Устанавливаейте приоритет для изучения и интервалы повторения, чтобы ускорить процесс.'}> </AppAboutItem>
-                </Row>
-            </Container>
+        <div className='about_block'>
+            <AppAboutItem 
+                pic={studyingPic} 
+                text={'World of Words - это приложение для изучения иностранных слов. Добавляйте и удаляйте слова, а также перевод к ним, пополняя словарь.'}> </AppAboutItem>
+            <AppAboutItem 
+                pic={filterPic} 
+                text={'Работать со словарем легко благодаря ряду фильтров - сортируйте слова по дате добавления и категории. '}> </AppAboutItem>
+            <AppAboutItem 
+                pic={freePic}
+                text={'Приложение абсолютно бесплатно - добавляйте любое количество слов и пользуйтесь словарем в любое время с разных устройств.'}> </AppAboutItem>
+            <AppAboutItem
+                pic={categoryPic}
+                text={'Устанавливаейте приоритет для изучения и интервалы повторения, чтобы ускорить процесс.'}> </AppAboutItem>
+        </div>
     ); 
 }
 
